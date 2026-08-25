a57c3e25fd196b12366a4b10a0e68b98d6226400
medhakg205 <medhakg12@gmail.com>
2026-08-25 23:53:48 +0530

    Language translation

---
+++ lib/app_settings.dart
@@ -0,0 +1,16 @@
+class AppSettings {
+  static String selectedLanguage = 'en';
+}
+
+const Map<String, String> languageCodes = {
+  'English': 'en',
+  'Hindi': 'hi',
+  'Tamil': 'ta',
+  'Telugu': 'te',
+  'Kannada': 'kn',
+  'Malayalam': 'ml',
+  'Marathi': 'mr',
+  'Bengali': 'bn',
+  'Gujarati': 'gu',
+  'Punjabi': 'pa',
+};
